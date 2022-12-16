bc4ebeca313581f66cd11ac95a8e206c928d76d4
B-loeb <bradleyloeb@gmail.com>
2022-12-16 00:17:39 -0500

    setting up server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-module.export = () => console.log('backend package')
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(5000, () => console.log(`Server started on port ${5000}`));
+
+
+
+
+
+// module.exports = () => console.log('backend package')
